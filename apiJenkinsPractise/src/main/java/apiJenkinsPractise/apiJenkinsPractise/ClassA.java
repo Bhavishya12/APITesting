fa8d56b8c6c8398adf4e0558c4e319121c0d25cc
computer <computer@user>
2020-05-03 13:03:47 +0530

    I ampushing the whole code

---
+++ apiJenkinsPractise/src/main/java/apiJenkinsPractise/apiJenkinsPractise/ClassA.java
@@ -0,0 +1,13 @@
+package apiJenkinsPractise.apiJenkinsPractise;
+
+import org.testng.annotations.Test;
+
+public class ClassA {
+	@Test
+	public void tc()
+	{
+		System.out.println("I am a Test Case");
+	}
+
+}
+ 
